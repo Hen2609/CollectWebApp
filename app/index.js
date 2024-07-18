c3d087a2b1cbf623d317f15ef6454852a99826e2
Hen2609 <chen.zilberman@gmail.com>
2024-07-18 17:11:43 +0300

    router composability init

--- app/index.js
+++ app/index.js
@@ -3,10 +3,13 @@ const app = express()
 
 const port = process.env.NODE_DOCKER_PORT || 8080;
 
+const api = require('./src/endpoints','/api')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/api', api)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
