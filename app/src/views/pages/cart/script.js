fc6e8cd3db61d20895eea7a859aa2d4e23121d6a
Hen2609 <chen.zilberman@gmail.com>
2024-08-01 17:48:29 +0300

    working signature pad

--- app/src/views/pages/cart/script.js
+++ app/src/views/pages/cart/script.js
@@ -22,3 +22,41 @@ async function initPage(){
 window.addEventListener('DOMContentLoaded', () => {
     initPage()
 })
+
+
+const canvas = $('#signature-pad')[0];
+const context = canvas.getContext('2d')
+context.lineWidth = 3;
+context.lineJoin = context.lineCap = 'round';
+
+let writingMode = false;
+function getCursorPosition(event){
+    const positionX = event.clientX - event.target.getBoundingClientRect().x;
+    const positionY = event.clientY - event.target.getBoundingClientRect().y;
+    return [positionX, positionY];
+}
+function handlePointerDown(event){
+    writingMode = true;
+    context.beginPath();
+    const [positionX, positionY] = getCursorPosition(event);
+    context.moveTo(positionX, positionY);
+}
+function handlePointerUp() {
+    writingMode = false;
+}
+function handlePointerMove(event){
+    if (!writingMode) return
+    const [positionX, positionY] = getCursorPosition(event);
+    context.lineTo(positionX, positionY);
+    context.stroke();
+}
+function getSigantureBase64(){
+    return canvas.toDataURL()
+}
+function clearPad(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+  
+canvas.addEventListener('pointerdown', handlePointerDown, {passive: true});
+canvas.addEventListener('pointerup', handlePointerUp, {passive: true});
+canvas.addEventListener('pointermove', handlePointerMove, {passive: true});
