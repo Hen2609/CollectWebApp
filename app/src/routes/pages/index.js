4c9d213fe2fe89e2ac98c2183e0ba0ecf75e9950
Hen2609 <chen.zilberman@gmail.com>
2024-09-14 17:57:43 +0300

    Add Mailgun integration and handle 404 in routes
    Introduced Mailgun integration for sending emails. Refactored homepage handler and added a catch-all route to handle 404 errors. Updated package-lock.json with new dependencies for Mailgun and session management.

--- app/src/routes/pages/index.js
+++ app/src/routes/pages/index.js
@@ -18,19 +18,9 @@ router.use("/privacy",privacyRoutes)
 router.use("/stats",statsRoutes)
 router.use("/orders",orderRoutes)
 
-router.get("/", async (req, res) => {
-    const categories = await categoriesController.getCategories()
-    let categories_filter = []
-    if(req.query.categories){
-        categories_filter = Array.isArray(req.query.categories) ? req.query.categories : [req.query.categories]
-    }
-    categoriesFilter = categories_filter.map(cat => mongoose.Types.ObjectId.createFromHexString(cat))
-    const search = req.query.search ? req.query.search : undefined
-    const products = await productsController.getProducts(search,categoriesFilter)
+router.get("/", handleHomepage)
 
-    render(req,res,'homepage','קודש SQUAD', {
-        categories,
-        products
-    })
+router.all("*", (req,res) => {
+    res.status(404).send('<h1>404! עמוד לא נמצא</h1>');
 })
 module.exports = router;
