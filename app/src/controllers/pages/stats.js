f67f6eb04fdedb140472ddbb2a6c8200e648bb0e
Hen2609 <chen.zilberman@gmail.com>
2024-09-14 17:41:31 +0300

    Add dedicated stats page controller
    Created a new `handleStatsPage` function in `pages/stats.js` to centralize the logic for rendering the stats page. Refactored the route to use the new controller function, ensuring cleaner and more maintainable code.

---
+++ app/src/controllers/pages/stats.js
@@ -0,0 +1,24 @@
+const {render} = require("../../utils/render")
+const {isAdminUser} = require("../../utils/auth");
+const {ordersByProducts, ordersByUsers} = require("../../services/order");
+
+/**
+ * Handles rendering of product page
+ * @async
+ * @function handleStatsPage
+ * @param {import("express").Request} req - The request object, containing the body data.
+ * @param {import("express").Response} res - The response object, used to send the response.
+ * @return {Promise<void>} A promise that resolves when the logout process completes.
+ */
+async function handleStatsPage(req, res){
+    if(!isAdminUser(req)){
+        return res.redirect('/')
+    }
+    const productOrders = await ordersByProducts()
+    const usersOrders   = await ordersByUsers()
+    render(req,res,'stats', 'סטטיסטיקה', {productOrders, usersOrders})
+}
+
+module.exports =  {
+    handleStatsPage
+}
--- app/src/routes/pages/stats.js
+++ app/src/routes/pages/stats.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {render} = require("../../utils/render")
-const orderController = require("../../controllers/api/order")
-const { isAdminUser } = require("../../utils/auth")
 
-router.get("/", async (req,res) => {
-    if(!isAdminUser(req)){
-        return res.redirect('/')
-    }    
-    const productOrders = await orderController.ordersByProducts()
-    const usersOrders   = await orderController.ordersByUsers()
-    render(req,res,'stats', 'סטטיסטיקה', {productOrders, usersOrders})
-})
+const {handleStatsPage} = require("../../controllers/pages/stats");
+
+router.get("/", handleStatsPage)
 
 module.exports = router;
